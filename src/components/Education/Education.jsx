a222e270fc427e7cb00e31f4757db93613e680d6
alissontfraga <alissontfraga@gmail.com>
2025-12-30 10:46:36 -0300

    feat: add education image for Harvard CS50x course

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -3,6 +3,7 @@ import Box from '../Box/Box';
 import EducationCard from '../EducationCard/EducationCard';
 import educationimg1 from '../../assets/images/educationimg1.png';
 import educationimg2 from '../../assets/images/educationimg2.png';
+import educationimg3 from '../../assets/images/educationimg3.png';
 
 
 const Education = () => {
@@ -10,6 +11,7 @@ const Education = () => {
     <section id="education">
         <Box title={'Education'} className={styles.educationBox}>
             <div className={styles.educationDisplay}>
+                <EducationCard image={educationimg3} title={"Harvard - Computer Science (CS50x)"}/>
                 <EducationCard image={educationimg1} title={"Cisco - Cyber Threat Management"}/>
                 <EducationCard image={educationimg2} title={"AWS - Cloud Foundations"}/>
                 <EducationCard title={"Anhanguera - Software Engineer (in progress)"}/>
